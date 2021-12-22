5926f553da6546dcda698616cf874dce3c6fec35
Raghav Bang <raghavbang2012@gmail.com>
2021-12-22 19:50:59 +0530

    Update and rename Build Array from Permutation to Build Array from Permutation.py

--- Array/Build Array from Permutation.py
+++ Array/Build Array from Permutation.py
@@ -1,4 +1,4 @@
-##Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it
+#Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it
 
 
 class Solution:
