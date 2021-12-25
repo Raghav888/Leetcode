3f8d9cbe9f0efee2ad2e5953d27e99cc2795e53d
Raghav Bang <raghavbang2012@gmail.com>
2021-12-25 23:00:18 +0530

    Update add-to-array-form-of-integer.py

--- add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,11 +1,9 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        ll=len(num)
-        base_value=10**(ll-1)
+        base_value=10**(len(num)-1)
         base_value=base_value*num[0]
-        for i in range(1,ll):
-            new_val=num[i]*(10**(ll-(1+i)))
-            base_value+=new_val
+        for i in range(1,len(num)):
+            base_value+=num[i]*(10**(len(num)-(1+i)))
         base_value+=k
         base_value=str(base_value)
         return list(map(int,base_value))
