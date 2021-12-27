7d4c153f08ff0e604fe7ff2a8d5ce8996141894a
Raghav Bang <raghavbang2012@gmail.com>
2021-12-27 22:22:50 +0530

    Time: 2240 ms (13.47%), Space: 14.1 MB (70.25%) - LeetHub

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0 or x==1:
+            return x
+        i=1
+        result=1
+        while(result<=x):
+            i+=1
+            result=i*i
+        return i-1
+        
